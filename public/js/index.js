c5eca56f7300ede2011db553e312a6ec0059d292
Maksim Golitsyn <maxim@infofree.ru>
2018-03-19 22:57:02 +0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,19 +2,10 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    from: 'Max',
-    text: 'Yup, that works fo me.'
-  })
 })
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server')
-})
-
-socket.on('newEmail', function (email) {
-  console.log('New email', email)
 })
 
 socket.on('newMessage', function (message) {
