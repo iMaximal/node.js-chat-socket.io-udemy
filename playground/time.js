87c2dfe58f7800b3cbc4b0a153f970f359af37ef
Maksim Golitsyn <maxim@infofree.ru>
2018-03-22 23:12:40 +0500

    play with moment js

---
+++ playground/time.js
@@ -0,0 +1,18 @@
+// :-))
+const moment = require('moment')
+
+const date = moment()
+console.log(date.format('hh:mm a'))
+
+
+//
+// const date = moment()
+// date.add(1, 'year').subtract(9, 'months')
+//
+// console.log(date.format('MMM Do, YYYY'))
+
+// const date = new Date()
+// const months = ['Jan', 'Feb']
+//
+// console.log(date.getMonth())
+//
