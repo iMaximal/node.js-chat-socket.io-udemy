b5d88796e2f3669ed915483afc07ed203031fb7d
Maksim Golitsyn <maxim@infofree.ru>
2018-03-24 14:01:15 +0500

    extend user, user.test

--- server/utils/users.js
+++ server/utils/users.js
@@ -18,6 +18,24 @@ class Users {
     this.users.push(user)
     return user
   }
+  removeUser(id) {
+    const user = this.getUser(id)
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id)
+    }
+
+    return user
+  }
+  getUser(id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList(room) {
+    const users = this.users.filter((user) => user.room === room)
+    const namesArray = users.map((user) => user.name)
+
+    return namesArray
+  }
 }
 
 module.exports = { Users }
